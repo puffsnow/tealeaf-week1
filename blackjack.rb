b086773f509c47042c59d37b8d61ca57670a279c
puffsnow <brucechen2@gmail.com>
2014-02-07 11:42:17 +0800

    turn the tab to spaces

--- blackjack.rb
+++ blackjack.rb
@@ -12,91 +12,92 @@ USED_DECK_COUNT = 3
 #say:
 #The program simulate a dealer, and every message from this program will call this function.
 def say msg
-	puts "@Dealer: #{msg}"
+  puts "@Dealer: #{msg}"
 end
+
 
 #result_declare:
 #The program will declare game result for every player. Message is depend on the result.
 def result_declare (name, result)
-	say "Great, #{name}! You win this game!" if result == "win"
-	say "Oops, sorry, #{name}. You lose this game." if result == "lose"
-	say "#{name}, we draw. :)" if result == "draw"
+  say "Great, #{name}! You win this game!" if result == "win"
+  say "Oops, sorry, #{name}. You lose this game." if result == "lose"
+  say "#{name}, we draw. :)" if result == "draw"
 end
 
 #shuffle:
 #This function will return an array that simulate deck which has been shuffled.
 def shuffle
-	shuffle_cards = []
+  shuffle_cards = []
 
-	USED_DECK_COUNT.times do |x|
-		i = 52
-		original_cards = Array.new(52) {|index| index}
-		until original_cards.empty?
-			taking_num = rand(i)
-			shuffle_cards.push(original_cards[taking_num])
-			original_cards.delete_at(taking_num)
-			i = i - 1
-		end
-	end
-	return shuffle_cards
+  USED_DECK_COUNT.times do |x|
+    i = 52
+    original_cards = Array.new(52) {|index| index}
+    until original_cards.empty?
+      taking_num = rand(i)
+      shuffle_cards.push(original_cards[taking_num])
+      original_cards.delete_at(taking_num)
+      i = i - 1
+    end
+  end
+  return shuffle_cards
 end
 
 #pick_card:
 #This function will pick a card from the deck in random and return the card number. Hmmm...so we have shuffled twice.
 def pick_card deck
-	taking_num = rand(deck.count)
-	return_value = deck[taking_num]
-	deck.delete_at(taking_num)
-	return return_value
+  taking_num = rand(deck.count)
+  return_value = deck[taking_num]
+  deck.delete_at(taking_num)
+  return return_value
 end
 
 #cards_to_point:
 #This function is count how many points is the cards in players hand.
 #It will count the max possible number(If A can be 11, it will be.), but don't be busted if it can avoid.
 def cards_to_point cards
-	
-	point = 0
-	cards_num = []
+  
+  point = 0
+  cards_num = []
 
-	cards.each { |card| cards_num.push(card % 13) }
+  cards.each { |card| cards_num.push(card % 13) }
 
-	cards_num.sort.reverse.each do |num|
-		if num >= 9
-			point = point + 10
-		elsif num > 0
-			point = point + num + 1
-		end
-	end
+  cards_num.sort.reverse.each do |num|
+    if num >= 9
+      point = point + 10
+    elsif num > 0
+      point = point + num + 1
+    end
+  end
 
-	a_count = cards_num.count(0)
+  a_count = cards_num.count(0)
 
-	point  = point + a_count
+  point  = point + a_count
 
-	a_count.times do
-		point = point + 10 if point <= 11
-	end
+  a_count.times do
+    point = point + 10 if point <= 11
+  end
 
-	return point
+  return point
 end
 
 #card_to_string:
 #Turn a number to string which is its real color and number.
 #From 0 to 51, it means club 1, club 2, club 3, ... spade K.
 def card_to_string card
-	colors = ["club", "diamond", "heart", "spade"]
-	numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
-	return colors[card / 13] + numbers[card % 13]
+  colors = ["club", "diamond", "heart", "spade"]
+  numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+  return colors[card / 13] + numbers[card % 13]
 end
 
 #cards_in_hand_to_string:
 #input: an array of number
 #output: a string which means which cards are they.
 def cards_in_hand_to_string cards
-	card_strings = []
-	cards.each do |card|
-		card_strings.push(card_to_string(card))
-	end
-	return card_strings.join(", ")
+  card_strings = []
+  cards.each do |card|
+    card_strings.push(card_to_string(card))
+  end
+  return card_strings.join(", ")
 end
 
 
@@ -120,22 +121,22 @@ say "How many players want join this game?"
 players_count = gets.chomp.to_i
 
 players_count.times do |x|
-	
-	player_num = x + 1
-	say "Please type player" + player_num.to_s + "'s name:"
-	input_name = gets.chomp
-	
-	if players_name.find_index(input_name)
-		puts "Sorry, the name is repeated, please check and run this program one more time;)"
-		exit
-	end
-	
-	players_name.push(input_name)
-	players_cards[input_name] = []
-	card = pick_card deck
-	players_cards[input_name].push(card)
-	card = pick_card deck
-	players_cards[input_name].push(card)
+  
+  player_num = x + 1
+  say "Please type player" + player_num.to_s + "'s name:"
+  input_name = gets.chomp
+  
+  if players_name.find_index(input_name)
+    puts "Sorry, the name is repeated, please check and run this program one more time;)"
+    exit
+  end
+  
+  players_name.push(input_name)
+  players_cards[input_name] = []
+  card = pick_card deck
+  players_cards[input_name].push(card)
+  card = pick_card deck
+  players_cards[input_name].push(card)
 
 end
 
@@ -145,77 +146,76 @@ card = pick_card deck
 dealer_cards.push(card)
 
 players_name.each do |player_name|
-	say "Hello! " + player_name + "! It's your turn!"
+  say "Hello! " + player_name + "! It's your turn!"
+  while true
+    player_point = cards_to_point players_cards[player_name]
+    say "Your current cards in hand are: " + cards_in_hand_to_string(players_cards[player_name])
+    say "Your current score is: " + player_point.to_s
 
-	while true
-		player_point = cards_to_point players_cards[player_name]
-		say "Your current cards in hand are: " + cards_in_hand_to_string(players_cards[player_name])
-		say "Your current score is: " + player_point.to_s
+    if player_point > 21
+      say "Oh! No! You busted!" 
+      break
+    elsif player_point == 21
+      say "Blackjack! You play a nice game!"
+      break
+    end
 
-		if player_point > 21
-			say "Oh! No! You busted!" 
-			break
-		elsif player_point == 21
-			say "Blackjack! You play a nice game!"
-			break
-		end
+    say "Do you want hit or stay?"
+    say "1) Hit  2)Stay"
+    command = gets.chomp.to_i
 
-		say "Do you want hit or stay?"
-		say "1) Hit  2)Stay"
+    if command == 2
+      say "OK, please wait..."
+      break
+    end
 
-		command = gets.chomp.to_i
+    card = pick_card deck
+    players_cards[player_name].push(card)
+  end
 
-		if command == 2
-			say "OK, please wait..."
-			break
-		end
-
-		card = pick_card deck
-		players_cards[player_name].push(card)
-	end
 end
 
 say "It's my turn."
 
 while true
-	
-	card = pick_card deck
-	dealer_cards.push card
+  
+  card = pick_card deck
+  dealer_cards.push card
 
-	dealer_point = cards_to_point dealer_cards
-	say "My current cards in hand are: " + cards_in_hand_to_string(dealer_cards)
-	say "My current score is: " + dealer_point.to_s
+  dealer_point = cards_to_point dealer_cards
+  say "My current cards in hand are: " + cards_in_hand_to_string(dealer_cards)
+  say "My current score is: " + dealer_point.to_s
 
-	if dealer_point > 21
-		say "Oh! No! I busted!"
-		break
-	elsif dealer_point == 21
-		say "Oh! Sorry! BLACKJACK! I win!;)"
-		break
-	elsif dealer_point > 17
-		say "I should stay..."
-		break
-	end
+  if dealer_point > 21
+    say "Oh! No! I busted!"
+    break
+  elsif dealer_point == 21
+    say "Oh! Sorry! BLACKJACK! I win!;)"
+    break
+  elsif dealer_point > 17
+    say "I should stay..."
+    break
+  end
 end
 
 players_name.each do |player_name|
-	player_point = cards_to_point players_cards[player_name]
+  player_point = cards_to_point players_cards[player_name]
 
-	if player_point > 21
-		result_declare(player_name, "lose")
-	elsif player_point == 21
-		result_declare(player_name, "win")
-	elsif dealer_point > 21
-		result_declare(player_name, "win")
-	elsif dealer_point == 21
-		result_declare(player_name, "lose")
-	elsif dealer_point > player_point
-		result_declare(player_name, "lose")
-	elsif dealer_point < player_point
-		result_declare(player_name, "win")
-	else
-		result_declare(player_name, "draw")
-	end
+  if player_point > 21
+    result_declare(player_name, "lose")
+  elsif player_point == 21
+    result_declare(player_name, "win")
+  elsif dealer_point > 21
+    result_declare(player_name, "win")
+  elsif dealer_point == 21
+    result_declare(player_name, "lose")
+  elsif dealer_point > player_point
+    result_declare(player_name, "lose")
+  elsif dealer_point < player_point
+    result_declare(player_name, "win")
+  else
+    result_declare(player_name, "draw")
+  end
 
 end
 
--- readme.md
+++ readme.md
@@ -7,26 +7,25 @@
 <p>
 Next step:
 <ol>
-	<li>don't just play once, player can play more times</li>
-	<li>maybe we can let player have chips?</li>
+  <li>don't just play once, player can play more times</li>
+  <li>maybe we can let player have chips?</li>
 </ol>
 </p>
 
 <p>
 14/02/05 It's first version. Here is some description about the blackjack.rb:<br />
 <ol>
-	<li>It support multiplayers, and it will remember all player's name</li>
-	<li>Players' name should not be conflict, or the program will shutdown</li>
-	<li>The game rule:
-	<ul>
-		<li>If player busted, player lose.(No matter what dealer's point is)</li>
-		<li>If player blackjack, player win.(No matter what dealer's point is)</li>
-		<li>If dealer busted, player win.</li>
-		<li>If dealer blackjack, player lose.</li>
-		<li>Then who's point is bigger, who win.</li>
-		<li>If player's point is equal to dealer's point, game draw.</li>
-	</ul>
-	</li>
+  <li>It support multiplayers, and it will remember all player's name</li>
+  <li>Players' name should not be conflict, or the program will shutdown</li>
+  <li>The game rule:
+  <ul>
+    <li>If player busted, player lose.(No matter what dealer's point is)</li>
+    <li>If player blackjack, player win.(No matter what dealer's point is)</li>
+    <li>If dealer busted, player win.</li>
+    <li>If dealer blackjack, player lose.</li>
+    <li>Then who's point is bigger, who win.</li>
+    <li>If player's point is equal to dealer's point, game draw.</li>
+  </ul>
+  </li>
 </ol>
 </p>
-
